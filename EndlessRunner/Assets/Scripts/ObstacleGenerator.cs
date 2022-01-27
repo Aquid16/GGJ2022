441e49fcaf8ff4a2f8c132f2345d61251c10fe2a
MYoshua64 <52865661+MYoshua64@users.noreply.github.com>
2022-01-27 23:09:13 +0200

    Object Pooler

---
+++ EndlessRunner/Assets/Scripts/ObstacleGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject obstaclePrefab;
+    [SerializeField] Transform obstacleParent;
+    [Space]
+    [Header("Spawn Parameters")]
+    [SerializeField] float minSpawnDistance = 4f;
+    [SerializeField] float maxSpawnDistance = 6f;
+
+    float prevXSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void SpawnNextObstacle()
+    {
+        float distance = Random.Range(minSpawnDistance, maxSpawnDistance);
+        GameObject obst = Instantiate(obstaclePrefab, new Vector3(prevXSpawn + distance, 0), Quaternion.identity, obstacleParent);
+
+    }
+}
