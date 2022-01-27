ded9a95301be72f81cb0dbf0e0e5aef1e5654924
MYoshua64 <52865661+MYoshua64@users.noreply.github.com>
2022-01-27 19:40:08 +0200

    Basic Player Swap

---
+++ EndlessRunner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer playerRenderer;
+
+    PlayerActions inputActions;
+
+    private void Awake()
+    {
+        inputActions = new PlayerActions();
+
+        inputActions.Gameplay.Swap.performed += ctx => Flip();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    void Flip()
+    {
+        Vector3 objScale = transform.localScale;
+        objScale.y *= -1;
+        transform.localScale = objScale;
+        playerRenderer.color = objScale.y == 1 ? Color.white : Color.black;
+    }
+}
