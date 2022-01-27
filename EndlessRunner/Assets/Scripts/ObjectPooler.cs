6f6a06cc6865b9c3785408fd5f67840be4129004
Aquid16 <54771966+Aquid16@users.noreply.github.com>
2022-01-28 00:15:08 +0200

    Object Pooler

--- EndlessRunner/Assets/Scripts/ObjectPooler.cs
+++ EndlessRunner/Assets/Scripts/ObjectPooler.cs
@@ -10,4 +10,32 @@ public class ObjectPooler : MonoBehaviour
         public GameObject prefab;
         public int number;
     }
+
+    [SerializeField] List<ItemData> poolData;
+    List<GameObject> pooledObjects = new List<GameObject>();
+    private void Start()
+    {
+        foreach (ItemData item in poolData)
+        {
+           for(int i=0;i<item.number;i++)
+            {
+                GameObject inst = Instantiate(item.prefab);
+                inst.SetActive(false);
+                pooledObjects.Add(inst);
+            }
+        }
+    }
+
+    public GameObject GetPooledItem()
+    {
+        for(int i=0; i<pooledObjects.Count-1; i++)
+        {
+            if(!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+
+        return null;
+    }
 }
